e910c8c86cf5cdd3427059e361651650725bf54d
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-22 00:41:46 +0500

    data-types

---
+++ 02-data-types/script.js
@@ -0,0 +1,29 @@
+// JS DATA TYPES 
+// 1. NUMBER 
+// let num = 10;
+// let floatNum = 2.3;
+
+// 2. STRING
+// let str = "hello";
+// let str2 = 'Hello';
+// let str3 = `Hello`;
+
+// convert string to number 
+// let str4 = "100";
+// console.log(typeof +str4);
+// console.log(typeof parseInt(str4));
+// console.log(typeof parseFloat(str4));
+
+// console.log(+"sam");  /* it gives NAN (not a number) */
+
+// 3. BOOLEAN 
+// console.log(typeof true);
+// console.log(typeof false);
+
+// 4. UNDEFINED 
+// let a;
+// console.log(a);
+
+// 5. NULL
+console.log(typeof null);
+console.log(typeof +null);
