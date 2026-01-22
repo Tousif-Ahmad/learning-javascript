817004f19114d9dab859062a051f5cd674aa8c24
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-22 18:20:48 +0500

    variables

---
+++ 03-variables/script.js
@@ -0,0 +1,20 @@
+// debugger
+console.log(firstName);
+
+var firstName = "uffexx";
+let lastName = "khan"
+let age = 23;
+const yearOfBirth = 2002;
+// let isHappy = true;
+
+// const hoursInday = 24;
+// hoursInday = 12;
+// console.log(hoursInday);
+
+// firstName = "Sam";
+// let userIntro = "Hi, I am " + firstName + " " + lastName + " I am " + age + " years old"
+
+// let a;
+// console.log(a);
+
+// school
