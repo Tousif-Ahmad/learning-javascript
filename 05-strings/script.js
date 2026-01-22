69d5fc0fd0627554bd6ae5b6a6f501060683f1cd
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-22 20:31:36 +0500

    strings

---
+++ 05-strings/script.js
@@ -0,0 +1,25 @@
+const message = "hello world!";
+const info = "i am tousif";
+const lastFourDigit = "2002";
+// console.log(message.length);
+// console.log(message.toLowerCase());
+// console.log(message.toUpperCase());
+// console.log(message.trim());
+// console.log(message.includes("hello"));
+// console.log(message.indexOf("o"));
+// console.log(message.lastIndexOf("o"));
+// console.log(message.replace("o", "a"));
+// console.log(message.replaceAll("o","a"));
+// console.log(message.concat(info));
+// console.log(message.concat(" i am khan", " i am a student"));
+// console.log(lastFourDigit.padStart(16, "*"));
+// console.log(lastFourDigit.padEnd(16, "*"));
+// console.log(message.charAt(1));
+// console.log(message.charCodeAt(0));
+
+// console.log(message.split(""));
+// console.log(message.split(" "));
+// console.log(message.split());
+
+// template literals 
+const templateStr = `last four digit of my account number is ${lastFourDigit}`
