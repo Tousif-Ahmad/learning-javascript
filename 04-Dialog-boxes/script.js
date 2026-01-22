1316b82da201e6b2c603d95f33c884d7435cf415
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-22 18:21:28 +0500

    dialog-boxes

---
+++ 04-Dialog-boxes/script.js
@@ -0,0 +1,4 @@
+// alert("Your message goes here..") /* it returns undefined  */
+// confirm("You would you like to proceed?"); /* it returns true or false */
+// let name = prompt("Enter your name ");  /* it returns null */
+// console.log(name);
