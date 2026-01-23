fbba1543256632d90e5303d7b3cdfa99a39aa241
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-23 22:06:35 +0500

    math object

---
+++ 06-Math-object/script.js
@@ -0,0 +1,54 @@
+// console.log("Math Object");
+
+// console.log(15 % 2);
+// console.log(15 / 2);
+// console.log(5 ** 2);
+
+//  MATH OBJECT 
+// console.log(Math.PI);
+// console.log(Math.SQRT2);
+// console.log(Math.sqrt(16));
+// console.log(Math.pow(5,2));
+
+// console.log(Math.floor(2.4));
+// console.log(Math.floor(2.9999999));
+// console.log(Math.floor(-2.5));
+
+// console.log(Math.ceil(2.11111));
+// console.log(Math.ceil(6.9871111));
+// console.log(Math.ceil(-3.1));
+
+// console.log(Math.round(2.4));
+// console.log(Math.round(2.5));
+
+// let ans = Math.floor(Math.random() * 11);
+// console.log(ans);
+
+// let result = Math.floor(Math.random() * 10 + 10)
+// console.log(result);
+
+// const max = 20;
+// const min = 10;
+// let result = Math.floor(Math.random() * (max-min+1) + min)
+// console.log(result);
+
+// INFINITY AND NAN 
+// console.log(7/0);
+// console.log(-8/0);
+// console.log(0/0);
+
+// TRUTHY AND FALSY VALUES 
+// falsy values => 0, -0, "", null, undefined, false,
+// baqii saari truthy values hain 
+
+
+// COMPARISON OPERATOR 
+// const user1 = "23";
+const user1 = 21;
+const user2 = 23;
+// console.log(user1 == user2); 
+// console.log(user1 === user2);
+// console.log(user1 != user2);
+// console.log(user1 !== user2);
+// console.log(user2 > user1);
+// console.log(user2 < user1);
