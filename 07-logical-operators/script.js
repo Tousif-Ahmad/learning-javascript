28181e8432a3757f9cd7f698641e309fd121f172
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-24 00:35:15 +0500

    logical-operarors

---
+++ 07-logical-operators/script.js
@@ -0,0 +1,19 @@
+// console.log("logical operators");
+let userAge = 23;
+
+const isCollegeStudent = (userAge >= 18) && (userAge <= 24);
+// console.log(isCollegeStudent);
+
+const isSchoolStudent = (userAge >= 5) && (userAge <= 18);
+// console.log(isSchoolStudent);
+
+const isStudent = isCollegeStudent || isSchoolStudent;
+// console.log(isStudent);
+
+// console.log(1 && 2);
+// console.log(0 || 1);
+
+// console.log(!null);
+// console.log(!false);
+// console.log(!true);
+
