4264c34358722ea633ef63bf774c3daa618d284b
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-25 01:11:00 +0500

    arrays

---
+++ 10-arrays/one.js
@@ -0,0 +1,48 @@
+const fruits = ["apple", "mango", "orange", "banana"];
+// console.log(typeof fruits);
+// fruits[4] = "banana";
+
+// console.log(fruits);
+// push : last elem add 
+// pop : last elem remove 
+// shift  : first se remove 
+// unshift : first pe add .
+//  
+
+
+// MULTIDIMENSIONAL ARRAY 
+// let arr = [["sam", 73], ["ali", 60]];
+// console.log(arr[0], arr[1]);
+
+// GREETING BOT 
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+let bot;
+bot = "teacherBot";
+
+let botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+
+bot = "professorBot";
+
+const nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+
+bot = "awesomeTeacherBot";
+
+const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+
+const favoriteSubject = "Computer Science";
+
+const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
+
+console.log("Well, it was nice to talk to you. Have a nice day!")
