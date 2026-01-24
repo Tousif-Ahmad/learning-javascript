1197a34ab4bcece832c70498ff585b885a19b66a
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-24 22:03:26 +0500

    objects

---
+++ 09-Objects/one.js
@@ -0,0 +1,31 @@
+let myName = "uffexx";
+let username1 = "sam";
+let username2 = "sam";
+
+let mystr1 = "";
+let mystr2 = "";
+
+const user = {
+    firstName : "uffex",
+    lastName : "khan",
+    age : 23,
+    education : 12,
+    city : "chitral",
+};
+const user2 = {
+    firstName : "uffex",
+    lastName : "khan",
+    // uffexx : "Developer",
+    address:{
+        city : "chitral",
+        pinCode : 234,
+        state : "KPK", 
+    }
+};
+// console.log(user2.lastName);
+// console.log(user2["lastName"]);
+// console.log(user2[myName]);
+
+// update objects 
+user2.age = 23;
+user2["isStudent"] = true;
---
+++ 09-Objects/two.js
@@ -0,0 +1,23 @@
+const user = {
+  firstName: 'Adarsh',
+  lastName: 'Singh',
+  pata: {
+    city: 'Bangalore',
+    pinCode: 876876,
+    state: 'Karnataka',
+    moreDetails: {
+      population: 9798897897,
+      area: '787 sq km',
+    },
+  },
+  age: 15,
+  isGraduate: false,
+}
+// console.log(user);
+
+// let username = "khan";
+// username = "uffexx";
+// console.log(username);
+
+// Object.seal(user);
+// Object.freeze(user);
