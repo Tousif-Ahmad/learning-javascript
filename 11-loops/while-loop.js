6c6a7b97ae29367714f55320fc9f94135f851f12
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-25 18:09:44 +0500

    loops

---
+++ 11-loops/while-loop.js
@@ -0,0 +1,22 @@
+// let num = 0;
+// while(num < 10){
+//     console.log(num);
+//     num++;
+// }
+
+// for loop 
+// for (let i = 1; i<=10; i++){
+//     console.log(i);
+// }
+
+// print even numbers from 0 to 100 
+// for (let i = 0; i<=100; i+=2){
+//     console.log(i);
+// }
+
+// do while loop 
+// let i = 10;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= 5)
