395c811d817c0830c8cd8ff3a20808f5a4b3e1f3
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-26 20:45:58 +0500

    arrow functions

---
+++ 12-functions/arrow-fnc.js
@@ -0,0 +1,20 @@
+// ARROW FUNCTIONS 
+
+// function declaration 
+function square(num){
+    return num * num
+}
+
+// function expression 
+// let square = function(num){
+//     return num * num;
+// }
+
+// arrow function expression 
+// let square = (num)=>{
+//     return num * num;
+// }
+
+
+// let square = (num)=>  num * num;
+
