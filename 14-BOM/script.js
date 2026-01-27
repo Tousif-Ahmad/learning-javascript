a18003812dffad832fdf15d59e8bf5e5fd35cd94
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-28 02:56:22 +0500

    BOM

---
+++ 14-BOM/script.js
@@ -0,0 +1,27 @@
+// BOM 
+// 1. location 
+// 2. reload 
+// 3. history
+// 4. innerHeight
+// 5. innerWidth
+// 6. open
+// 7. close 
+// 8. resizeBy 
+// 9. resizeTo /* condition it should use open, and resizable  */
+// 10. scrollTo
+// 11. print
+// 12. document 
+
+// console.log(window);
+
+// var a = 5;
+// let b = 10;
+
+// console.log(window.a);
+// console.log(window.b);
+
+
+// console.log(this === window);
+
+// location.href = location.href;
+
