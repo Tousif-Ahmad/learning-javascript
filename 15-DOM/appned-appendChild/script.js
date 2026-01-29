2ca7a3533e827682c58fbbd558d6e2160cf632fc
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-29 17:43:01 +0500

    append and appendChild

---
+++ 15-DOM/appned-appendChild/script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+const card = document.querySelector(".card");
+const h1 = document.querySelector("h1");
+
+// container.appendChild(h1)
+// h1.cloneNode(true)
+// container.appendChild(h1.cloneNode(true))
+
+// container.appendChild(card.cloneNode(true))
+
+// for (let i = 2; i<=100; i++){
+//     let newCard = card.cloneNode();
+//     newCard.textContent = i;
+//     container.appendChild(newCard);
+// }
