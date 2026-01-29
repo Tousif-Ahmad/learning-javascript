f83f06fa79311a8cf229244ec057697ac66d8eff
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-29 17:42:14 +0500

    creating elements

---
+++ 15-DOM/create-element/script.js
@@ -0,0 +1,35 @@
+const h1 = document.querySelector('h1')
+const container = document.querySelector(".container")
+
+// let para = document.createElement("p");
+// para.innerText = "this is a paragraph";
+// para.classList.add("my-para")
+// container.append(para)
+
+// let imgElement = document.createElement("img");
+// imgElement.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
+// container.append(imgElement)
+
+
+// for (let i = 2; i<=100; i++){
+//     const newImg = document.createElement("img");
+//     newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+//     container.append(newImg)
+// }
+// let img = document.querySelector(".img");
+// container.appendChild(h1)
+// container.appendChild(h1.cloneNode(true))
+
+// for (let i = 2; i <=100; i++) {
+//     const newCard = card.cloneNode()
+//     newCard.innerText = i
+//     container.append(newCard)
+// }
+
+// for (let i = 2; i<=100; i++){
+//     const newImg = img.cloneNode();
+//     newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`
+//     container.append(newImg)
+// }
+
+
