9bad218ff8bcbf2feac3764028e0051821b34d53
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-01-30 00:30:04 +0500

    mouse events

---
+++ 15-DOM/mouse-events/script.js
@@ -0,0 +1,61 @@
+const h1 = document.querySelector('h1')
+const addButton = document.querySelector('.card')
+const container = document.querySelector('.container')
+
+let count = 0
+
+// card.addEventListener('mousedown', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+// card.addEventListener('mouseup', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+// addButton.addEventListener('mouseenter', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addButton.addEventListener('mouseleave', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addButton.addEventListener('mousemove', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addButton.addEventListener('mouseout', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// addButton.addEventListener('mouseover', (e) => {
+//     console.log(e);
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+
