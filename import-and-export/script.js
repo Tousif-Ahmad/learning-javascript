4651b31442755b939ba138f0b2aeaa133736948d
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2026-02-05 21:56:17 +0500

    import and export

---
+++ import-and-export/script.js
@@ -0,0 +1,10 @@
+// console.log("Import and Export in Js");
+
+
+
+import { usersData, myName } from "./usersData";
+import productsData from "./productsData";
+
+console.log(productsData);
+console.log(usersData);
+console.log(myName);
